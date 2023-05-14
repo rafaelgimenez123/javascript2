ce04d9de8da1a47fe8d3b480f8a97c66b37ac29c
rafaelgimenez123 <107961108+rafaelgimenez123@users.noreply.github.com>
2023-05-14 15:56:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+
+const alumno_nombre = document.getElementById("alumno_nombre");
+const alumno_curso = document.getElementById("alumno_curso");
+const alumno_edad = document.getElementById("alumno_edad");
+const alumno_descripcion = document.getElementById("alumno_descripcion");
+const contenedor_alumno = document.getElementById("contenedor_alumnos");
+
 class Alumno {
   constructor(nombre, curso, edad, descripcion) {
     this.nombre = nombre;
@@ -6,43 +13,35 @@ class Alumno {
     this.descripcion = descripcion;
   }
 }
+fetch("./alumnos.json")
+  .then(data => data.json())
+  .then(alumnos => {
+    const contenedor_alumno = document.getElementById("contenedor_alumnos");
+    
+  
+    alumnos.forEach((alumno) => {
+      const Alumno_element = document.createElement("div");
+      Alumno_element.classList.add("card");
+      Alumno_element.innerHTML = `
+        <div class="container_card">
+          <h3>${alumno.nombre}</h3>
+          <ul>
+            <li>Edad alumno: ${alumno.edad}</li>
+            <li>Curso alumno: ${alumno.curso}</li>
+            <li>Descripción alumno: ${alumno.descripcion}</li>
+          </ul>
+        </div>
+      `;
+  
+      contenedor_alumno.appendChild(Alumno_element);})
+  });
 
-const alumnosPrefabricados = [{
-  nombre: "Juan Pérez",
-  curso: "3er año",
-  edad: "20 años",
-  descripcion: "Estudiante destacado en matematicas"
-},
-{
-  nombre: "Luca lopez",
-  curso: "3er año",
-  edad: "23 años",
-  descripcion: "Estudiante destacado en sociologia"
-},
-{
-  nombre: "Jimmy donnaldson",
-  curso: "3er año",
-  edad: "22 años",
-  descripcion: "Estudiante destacado en fisica"
-},
-{
-  nombre: "Luca lopez",
-  curso: "3er año",
-  edad: "19 años",
-  descripcion: "Estudiante destacado en Quimica"
-}
-];
+
 
 const CrearAlumno = (event) => {
-  event.preventDefault();
-
-  const alumno_nombre = document.getElementById("alumno_nombre");
-  const alumno_curso = document.getElementById("alumno_curso");
-  const alumno_edad = document.getElementById("alumno_edad");
-  const alumno_descripcion = document.getElementById("alumno_descripcion");
-  const contenedor_alumno = document.getElementById("contenedor_alumnos");
 
   if (![alumno_nombre, alumno_curso, alumno_edad, alumno_descripcion].every((element) => element.value)) {
+    event.preventDefault();
     Swal.fire('Ingresa todos los campos para continuar');
     return;
   }
@@ -60,22 +59,10 @@ const CrearAlumno = (event) => {
 
   const Alumno_element = document.createElement("div");
   Alumno_element.classList.add("card");
-  Alumno_element.innerHTML = `
-    <div class="container_card">
-      <h3>${nuevoAlumno.nombre}</h3>
-      <ul>
-        <li>Edad alumno: ${nuevoAlumno.edad}</li>
-        <li>Curso alumno: ${nuevoAlumno.curso}</li>
-        <li>Descripción alumno: ${nuevoAlumno.descripcion}</li>
-      </ul>
-    </div>
-  `;
-
   contenedor_alumno.appendChild(Alumno_element);
 };
 
-
-const mostrarAlumnos = () => {
+function mostrarAlumnos() {
   const contenedor_alumno = document.getElementById("contenedor_alumnos");
   const listaAlumnos = JSON.parse(localStorage.getItem('alumnos')) || [];
 
@@ -95,10 +82,9 @@ const mostrarAlumnos = () => {
 
     contenedor_alumno.appendChild(Alumno_element);
   });
-};
+}
 
 const form = document.getElementById("submit_button_form");
 form.addEventListener("submit", CrearAlumno);
 
 mostrarAlumnos();
-
